a7da4b9d2684f1b090d08634ff75b3e1fb2e4748
Dan Bowles <daniel.bowles@gmail.com>
2024-03-12 16:07:38 -0400

    :bug: Fixing LEDs on saegewerk

--- keyboards/klor/config.h
+++ keyboards/klor/config.h
@@ -17,18 +17,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-
 // ┌─────────────────────────────────────────────────┐
 // │ k e y   m a t r i x                             │
 // └─────────────────────────────────────────────────┘
 
-#define MATRIX_ROW_PINS { C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS \
+    { C6, D7, E6, B4 }
+#define MATRIX_COL_PINS \
+    { F6, F7, B1, B3, B2, B6 }
 #define DIODE_DIRECTION COL2ROW
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 6
 #define DEBOUNCE 5
-
 
 // ┌─────────────────────────────────────────────────┐
 // │ s p l i t   c o m m u n i c a t i o n           │
@@ -37,20 +37,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 #define SOFT_SERIAL_PIN D2
 #define SERIAL_USE_MULTI_TRANSACTION
-//#define SPLIT_LAYER_STATE_ENABLE
-//#define SPLIT_USB_DETECT
-
+// #define SPLIT_LAYER_STATE_ENABLE
+// #define SPLIT_USB_DETECT
 
 // ┌─────────────────────────────────────────────────┐
 // │ e n c o d e r s                                 │
 // └─────────────────────────────────────────────────┘
 
-#define ENCODERS_PAD_A       { F5 }
-#define ENCODERS_PAD_B       { F4 }
-#define ENCODERS_PAD_A_RIGHT { F4 }
-#define ENCODERS_PAD_B_RIGHT { F5 }
+#define ENCODERS_PAD_A \
+    { F5 }
+#define ENCODERS_PAD_B \
+    { F4 }
+#define ENCODERS_PAD_A_RIGHT \
+    { F4 }
+#define ENCODERS_PAD_B_RIGHT \
+    { F5 }
 #define ENCODER_RESOLUTION 2
-
 
 // ┌─────────────────────────────────────────────────┐
 // │ o l e d s                                       │
@@ -59,31 +61,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef OLED_ENABLE
 #    define OLED_DISPLAY_128X64
 #    define SPLIT_OLED_ENABLE
-#    define OLED_FONT_H  "./lib/glcdfont.c"
+#    define OLED_FONT_H "./lib/glcdfont.c"
 #endif
-
 
 // ┌─────────────────────────────────────────────────┐
 // │ r g b   l e d s                                 │
 // └─────────────────────────────────────────────────┘
 
 #define WS2812_DI_PIN D3
-#define RGBLED_NUM 42
-#define RGBLED_SPLIT { 21, 21 }
+#define RGBLED_NUM 36
+#define RGBLED_SPLIT \
+    { 18, 18 }
+// #define RGBLED_NUM 42
+// #define RGBLED_SPLIT { 21, 21 }
 #define RGB_MATRIX_LED_COUNT RGBLED_NUM
 
 #ifdef RGB_MATRIX_ENABLE
 #    define SPLIT_TRANSPORT_MIRROR
-#    define DRIVER_LED_TOTAL 42 // Number of LEDs
-#    define RGB_MATRIX_SPLIT { 21, 21 }
+#    define DRIVER_LED_TOTAL 36 // Number of LEDs
+#    define RGB_MATRIX_SPLIT \
+        { 18, 18 }
+// #    define DRIVER_LED_TOTAL 42 // Number of LEDs
+// #    define RGB_MATRIX_SPLIT { 21, 21 }
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
 #    define RGB_MATRIX_STARTUP_HUE 35
 #    define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGB_MATRIX_KEYPRESSES
 #endif
-
-
 
 // ┌─────────────────────────────────────────────────┐
 // │ a u d i o                                       │
@@ -104,7 +109,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #endif
 
-
 // ┌─────────────────────────────────────────────────┐
 // │ h a p t i c   f e e d b a c k                   │
 // └─────────────────────────────────────────────────┘
@@ -112,14 +116,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef HAPTIC_ENABLE
 #    define FB_ERM_LRA 1
 #    define FB_BRAKEFACTOR 3 // For 1x:0, 2x:1, 3x:2, 4x:3, 6x:4, 8x:5, 16x:6, Disable Braking:7
-#    define FB_LOOPGAIN 1 // For  Low:0, Medium:1, High:2, Very High:3
+#    define FB_LOOPGAIN 1    // For  Low:0, Medium:1, High:2, Very High:3
 #    define RATED_VOLTAGE 2
 #    define V_PEAK 2.8
 #    define V_RMS 2.0
 #    define F_LRA 150 // resonance freq
-#    define DRV_GREETING       alert_750ms
+#    define DRV_GREETING alert_750ms
 #endif
-
 
 // ┌─────────────────────────────────────────────────┐
 // │ t r a c k b a l l                               │
